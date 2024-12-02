f1870f7b40addb00de62ed72810dc6de3ba8212c
stanleylin924 <stanleylin924@gmail.com>
2024-12-02 23:10:45 +0800

    Modify Path Tracer settings.

--- scripts/PathTracer.py
+++ scripts/PathTracer.py
@@ -2,13 +2,13 @@ from falcor import *
 
 def render_graph_PathTracer():
     g = RenderGraph("PathTracer")
-    PathTracer = createPass("PathTracer", {'samplesPerPixel': 1})
+    PathTracer = createPass("PathTracer", {'samplesPerPixel': 1, 'useNRDDemodulation': False})
     g.addPass(PathTracer, "PathTracer")
-    VBufferRT = createPass("VBufferRT", {'samplePattern': 'Stratified', 'sampleCount': 16, 'useAlphaTest': True})
+    VBufferRT = createPass("VBufferRT", {'outputSize': 'Default', 'samplePattern': 'Center', 'sampleCount': 16, 'useAlphaTest': True, 'adjustShadingNormals': True, 'forceCullMode': False, 'cull': 'Back', 'useTraceRayInline': False, 'useDOF': False})
     g.addPass(VBufferRT, "VBufferRT")
-    AccumulatePass = createPass("AccumulatePass", {'enabled': True, 'precisionMode': 'Single'})
+    AccumulatePass = createPass("AccumulatePass", {'enabled': True, 'outputSize': 'Default', 'autoReset': True, 'precisionMode': 'Single', 'maxFrameCount': 0, 'overflowMode': 'Stop'})
     g.addPass(AccumulatePass, "AccumulatePass")
-    ToneMapper = createPass("ToneMapper", {'autoExposure': False, 'exposureCompensation': 0.0})
+    ToneMapper = createPass("ToneMapper", {'outputSize': 'Default', 'useSceneMetadata': True, 'exposureCompensation': 0.0, 'autoExposure': False, 'filmSpeed': 100.0, 'whiteBalance': False, 'whitePoint': 6500.0, 'operator': 'Linear', 'clamp': True, 'whiteMaxLuminance': 1.0, 'whiteScale': 11.199999809265137, 'fNumber': 1.0, 'shutter': 1.0, 'exposureMode': 'AperturePriority'})
     g.addPass(ToneMapper, "ToneMapper")
     g.addEdge("VBufferRT.vbuffer", "PathTracer.vbuffer")
     g.addEdge("VBufferRT.viewW", "PathTracer.viewW")
