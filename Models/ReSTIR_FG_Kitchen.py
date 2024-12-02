081557f9c3fa3169599096ed1f2044d3ed5751f4
stanleylin924 <stanleylin924@gmail.com>
2024-12-03 00:26:22 +0800

    Modify ReSTIR FG settings.
    (cherry picked from commit 35af67f8804ecaeb2e7b035a24df4bf6cff8c7c2)

--- Models/ReSTIR_FG_Kitchen.py
+++ Models/ReSTIR_FG_Kitchen.py
@@ -11,7 +11,7 @@ def render_graph_ReSTIR_FG():
     g = RenderGraph('ReSTIR_FG')
     g.create_pass('AccumulatePass', 'AccumulatePass', {'enabled': False, 'outputSize': 'Default', 'autoReset': True, 'precisionMode': 'Single', 'maxFrameCount': 0, 'overflowMode': 'Stop'})
     g.create_pass('ToneMapper', 'ToneMapper', {'outputSize': 'Default', 'useSceneMetadata': True, 'exposureCompensation': 0.0, 'autoExposure': False, 'filmSpeed': 100.0, 'whiteBalance': False, 'whitePoint': 6500.0, 'operator': 'Linear', 'clamp': True, 'whiteMaxLuminance': 1.0, 'whiteScale': 11.199999809265137, 'fNumber': 1.0, 'shutter': 1.0, 'exposureMode': 'AperturePriority'})
-    g.create_pass('VBufferRT', 'VBufferRT', {'outputSize': 'Default', 'samplePattern': 'Center', 'sampleCount': 16, 'useAlphaTest': True, 'adjustShadingNormals': True, 'forceCullMode': False, 'cull': 'Back', 'useTraceRayInline': False, 'useDOF': True})
+    g.create_pass('VBufferRT', 'VBufferRT', {'outputSize': 'Default', 'samplePattern': 'Center', 'sampleCount': 16, 'useAlphaTest': True, 'adjustShadingNormals': True, 'forceCullMode': False, 'cull': 'Back', 'useTraceRayInline': False, 'useDOF': False})
     g.create_pass('ReSTIR_FG', 'ReSTIR_FG', {'PhotonBufferSizeGlobal': 800000, 'PhotonBufferSizeCaustic': 400000, 'AnalyticEmissiveRatio': 0.3499999940395355, 'PhotonBouncesGlobal': 10, 'PhotonBouncesCaustic': 10, 'PhotonRadiusGlobal': 0.01600000075995922, 'PhotonRadiusCaustic': 0.004000000189989805, 'EnableStochCollect': True, 'StochCollectK': 3, 'EnablePhotonCullingGlobal': True, 'EnablePhotonCullingCaustic': True, 'CullingRadius': 0.10000000149011612, 'CullingBits': 20, 'CausticCollectionMode': 3, 'CausticResamplingMode': 2, 'EnableDynamicDispatch': False, 'NumDispatchedPhotons': 1199616})
     g.add_edge('AccumulatePass.output', 'ToneMapper.src')
     g.add_edge('VBufferRT.mvec', 'ReSTIR_FG.mvec')
@@ -23,7 +23,7 @@ def render_graph_ReSTIR_FG():
 m.addGraph(render_graph_ReSTIR_FG())
 
 # Scene
-m.loadScene('D:/3D_Scene/ReSTIR-FG/Kitchen_ReSTIRFG/KitchenReSTIRFG_v1.1.pyscene')
+m.loadScene('D:/3D_Scene/ReSTIR-FG/Kitchen_ReSTIRFG/KitchenReSTIRFG_v1.3.pyscene')
 m.scene.renderSettings = SceneRenderSettings(useEnvLight=True, useAnalyticLights=True, useEmissiveLights=True, useGridVolumes=True, diffuseAlbedoMultiplier=1)
 m.scene.cameraSpeed = 1.0
 
@@ -33,7 +33,7 @@ m.ui = True
 
 # Clock Settings
 m.clock.time = 0
-m.clock.framerate = 0
+m.clock.framerate = 30
 # If framerate is not zero, you can use the frame property to set the start frame
 # m.clock.frame = 0
 
@@ -42,4 +42,4 @@ m.frameCapture.outputDir = 'D:/Temp/FrameCapture'
 m.frameCapture.baseFilename = 'Mogwai'
 
 # framecapture.capture_cameras(m, 30)
-# framecapture.capture_frames(m, 0, 100)
+# framecapture.capture_frames(m, 0, 1000)
